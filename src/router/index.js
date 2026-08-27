64c50be44230217b863e5ad2dd886ceab305432d
hong <bubu5437@gmail.com>
2026-08-27 14:19:05 +0800

    buildagain

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ import AnchorEditor from '../views/AnchorEditor.vue'
 
 const router = createRouter({
 
-history:createWebHistory(),
+history:createWebHistory(import.meta.env.BASE_URL),
 
 routes:[
 
